7fc100390881dafb0c14d06b6e24d69283ec591e
shivam260304 <shivam260304@gmail.com>
2025-01-10 19:31:57 +0530

    1:17 done day 3

--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,3 +1,21 @@
 // This page has all the logic for frontend index.ejs
 
 const socket = io();
+const chess = new Chess();
+const boardElement = document.querySelector('.chessboard');
+
+let draggedPiece = null;
+let sourceSquare = null;
+let playerRole = null;
+
+const renderBoard =() =>{
+    const board = chess.board();
+    boardElement.innerHTML = ''; // agr kuch hai is element me to pehle usse khali kardo
+    console.log(board); // print board structure in conosle of client;
+}
+
+const handleMove = () => {};
+
+const getPieceUnicode = () => {};
+
+renderBoard();
